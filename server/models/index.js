47ff88a37672ed64b636b8ce79d7b70315794f83
tonysm-10 <ayalaarellanoanthony@gmail.com>
2023-06-26 17:55:43 -0700

    created the react app client and the server folder with the neccessary folders

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+const User = require('./User');
+const Cart = require('./Cart');
+const Product = require('./Product')
+const Order = require('./Order')
+
+module.exports = { User, Cart, Product, Order };
+
