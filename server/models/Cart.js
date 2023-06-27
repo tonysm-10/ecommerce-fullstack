3db3af47a707520deb13bd98941c53b1b049b300
tonysm-10 <ayalaarellanoanthony@gmail.com>
2023-06-26 21:08:42 -0700

    finished the Cart and the Product models

---
+++ server/models/Cart.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const cartSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    products: [
+        {
+            product: {
+                type: Schema.Types.ObjectId,
+                ref: 'Product',
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true
+            }
+        }
+    ]
+});
+
+const Cart = model('Cart', cartSchema);
+
+module.exports = Cart;
